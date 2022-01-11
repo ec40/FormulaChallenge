8c3e3f1e21add77fc64723e3c6a46ec7251afb7e
ec40 <76906306+ec40@users.noreply.github.com>
2022-01-11 13:50:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ ops_str = [" + ", " - ", "/", " x "]
 
 
 class FormChallenge:
-
     def __init__(self, no_digs, int_range, digs=None):
         self.no_digs = no_digs
         self.int_range = int_range
@@ -35,16 +34,15 @@ class FormChallenge:
 
         for operations in range(4):
             for k in range(len(self.digs)):
-                if self.digs[k] in self.ints and self.digs[k] not in self.form_dict.values():
+                if self.digs[k] in self.ints and self.digs[
+                        k] not in self.form_dict.values():
                     self.form_dict[str(self.digs[k])] = self.digs[k]
                 for j in range(len(running_keys)):
                     result = ops[operations](running_list[j], self.digs[k])
-                    self.running_dict[
-                        "(" + str(running_keys[j]) + ops_str[operations] + str(self.digs[k]) + ")"] = result
+                    self.running_dict["(" + str(running_keys[j]) + ops_str[operations] + str(self.digs[k]) + ")"] = result
                     if result in self.ints and result not in self.form_dict.values():
-                        self.form_dict[
-                            "(" + str(running_keys[j]) + ops_str[operations] + str(self.digs[k]) + ")"] = result
-                    if len(self.form_dict) >= self.int_range+1:
+                        self.form_dict["(" + str(running_keys[j]) + ops_str[operations] +str(self.digs[k]) + ")"] = result
+                    if len(self.form_dict) >= self.int_range + 1:
                         break
 
         self.running_dict = trimDict(self.running_dict)
@@ -59,12 +57,11 @@ class FormChallenge:
     def getDigsStr(self):
         digits = ""
         for i in self.digs:
-            digits = digits + str(i)
+            digits = digits + str(i) + " "
         return digits
-    
+
 def trimDict(d):
 
-    #for timming duplicate formula results in each iteration
     new_d = {}
     temp = []
     for key, val in d.items():
@@ -73,13 +70,12 @@ def trimDict(d):
             new_d[key] = int(val)
     return new_d
 
-
 def Run(no_digs, first_x_integers, digits=None):
 
     temp_dict = {}
     if digits:
         for dig in digits:
-             temp_dict[dig] = dig
+            temp_dict[dig] = dig
         x = FormChallenge(no_digs, first_x_integers, digits)
         x.running_dict = temp_dict
     else:
@@ -95,7 +91,7 @@ def Run(no_digs, first_x_integers, digits=None):
         output = x.getFormDict()
         count += 1
 
-        if len(output.index) >= first_x_integers+1:
+        if len(output.index) >= first_x_integers + 1:
 
             print("operators used:", count)
             print("digits used:", x.digs)
@@ -110,11 +106,29 @@ def Run(no_digs, first_x_integers, digits=None):
         else:
             i = x.getForms(i)
 
+    while True:
+      try:
+        check_in = float(input("x to exit, get any formula generated>> "))
+        if check_in == 'x' or check_in == 'X':
+          break
+        elif check_in in range(first_x_integers):
+          for key, val in i.items():
+            if float(check_in) == val:
+              print(key)
+        else:
+          print("outside range")
+          for key, val in i.items():
+            if float(check_in) == val:
+              print("rough formula: "+key)
+      except:
+        print("Try again!")
+
+
 def main():
     f = open("output.csv", "w")
     f.truncate()
     f.close()
-    
+
     int_range = int(input("What number would you like to go up to? "))
 
     no_digs = 0
@@ -125,23 +139,20 @@ def main():
             no_digs = int(input("Enter number of digits(1-9) "))
 
     while True:
-        
+
         j = input("Would you like to pick your digits?(y/n) ")
         if j == "y" or j == "Y":
             digs = []
             y = 1
             while y <= no_digs:
-                d = int(input("Enter digit "+str(y)+" "))
+                d = int(input("Enter digit " + str(y) + " "))
                 digs.append(d)
                 y += 1
-            Run(no_digs,int_range, digs)
+            Run(no_digs, int_range, digs)
             break
         elif j == "n" or j == "N":
-            check = True
             Run(no_digs, int_range)
             break
 
+
 main()
-
-
-
